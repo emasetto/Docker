dfd91b2a7f5972079125b95bb96de449648004c5
emasetto <edhercarvalho@hotmail.com>
2022-09-12 21:29:21 -0300

    dockerfile

--- aulabd.py
+++ aulabd.py
@@ -1,15 +1,28 @@
 import os
 from flask import Flask, request, render_template
 from flaskext.mysql import MySQL
+import pyodbc
 
 mysql = MySQL()
 app = Flask(__name__)
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'edher123'
-app.config['MYSQL_DATABASE_DB'] = 'teste'
-app.config['MYSQL_DATABASE_HOST'] = '172.17.0.0'
-mysql.init_app(app)
+server_endpoint = 'DESKTOP-MRESNQ3\SQLEXPRESS'
+porta = ''
+db_name ='impacta'
+db_username = 'user_name'
+db_password = 'user_username'
+driver = 'SQL Server'
+
+#setup de conexão com db
+def_conn = f'DRIVER={driver};SERVER={server_endpoint};DATABASE={db_name};UID={db_username};PWD={db_password}'
+cnn = pyodbc.connect(def_conn)
+cursor = cnn.cursor()
+
+# app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'edher123'
+# app.config['MYSQL_DATABASE_DB'] = 'tbl_user'
+# app.config['MYSQL_DATABASE_HOST'] = '172.17.0.0'
+# mysql.init_app(app)
 
 @app.route('/')
 def main():
